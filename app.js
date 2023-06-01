839f77fbfad6df1dee37c8eea2b1da8a7bfb0e61
tinaess09 <fbda22-014@thuto.bac.ac.bw>
2023-06-01 21:20:38 +0200

    ADD NEW CHANGES

---
+++ app.js
@@ -0,0 +1,8 @@
+     <!-- Footer -->
+    <footer>
+      <div class="container-fluid text-center">
+        <p>&copy; 2021 Movie Webpage. All Rights Reserved.</p>
+      </div>
+    </footer>
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
